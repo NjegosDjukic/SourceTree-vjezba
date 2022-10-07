321c8b8fe198ae292515372a57234769a2e7f751
DESKTOP-O8I5G7C\NjegosDjukic <djukicnjegos9@gmail.com>
2022-10-07 14:36:13 +0200

    e

---
+++ src/components/ComponentC.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const ComponentC = () => {
+  return (
+    <div className='item2'>
+        test
+    </div>
+  )
+}
+export default ComponentC
---
+++ src/components/ComponentD.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ComponentD = () => {
+  return (
+    <div className='item3'>ComponentD</div>
+  )
+}
+
+export default ComponentD
